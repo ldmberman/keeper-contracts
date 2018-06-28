17dc22141499ff1b31e35edd816d42a8c2b240a2
ssallam <samer@oceanprotocol.com>
2018-06-28 16:28:01 +0200

    Update truffle config to deploy to ocean test net (remote and local).

--- truffle.js
+++ truffle.js
@@ -5,7 +5,21 @@ module.exports = {
             host: 'localhost',
             port: 8545,
             network_id: '*',
-            gas: 6000000
+            gas: 6000000,
+        },
+        ocean_poa_net_local: {
+            host: 'localhost',
+            port: 8545,
+            network_id: '*',
+            gas: 6000000,
+            from: "0x00bd138abd70e2f00903268f3db08f2d25677c9e"
+        },
+        ocean_poa_net: {
+            host: '40.115.16.244',
+            port: 8545,
+            network_id: '*',
+            gas: 6000000,
+            from: "0x00bd138abd70e2f00903268f3db08f2d25677c9e"
         },
     },
     solc: {
