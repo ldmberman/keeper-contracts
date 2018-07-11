a6ea89d27666ba4d795410072a6e874eb92c6fee
gongf05 <fang.ucla@gmail.com>
2018-07-11 12:12:18 -0700

    fix JS linting warning

--- test/TestUniqueId.js
+++ test/TestUniqueId.js
@@ -2,8 +2,10 @@
 /* eslint-disable no-console, max-len */
 
 const Market = artifacts.require('Market.sol')
-var Web3 = require("web3");
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+const Web3 = require('web3')
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
 
 contract('Market', (accounts) => {
     describe('Test Unique Id', () => {
@@ -14,20 +16,20 @@ contract('Market', (accounts) => {
             const str = 'data asset'
             console.log('input string : ', str)
             console.log('====== generate id from input string =======')
-            soid = await market.generateStr2Id(str)
+            const soid = await market.generateStr2Id(str, { from: accounts[0] })
             console.log('solidity: id from string : ', soid)
 
-            web3id = web3.sha3(str)
+            const web3id = web3.sha3(str)
             console.log('web3: id from string : ', web3id)
             assert.strictEqual(soid, web3id, 'Two unique Id methods shall generate the same hash')
 
             console.log('====== generate id from input bytes =======')
             const byt = '0x7ace91f25e0838f9ed7ae259670bdf4156b3d82a76db72092f1baf06f31f5038'
             console.log('input bytes : ', byt)
-            soid2 = await market.generateBytes2Id(byt)
+            const soid2 = await market.generateBytes2Id(byt, { from: accounts[0] })
             console.log('solidity: id from bytes : ', soid2)
 
-            web3id2 = web3.sha3(byt, {encoding: 'hex'})
+            const web3id2 = web3.sha3(byt, { encoding: 'hex' })
             console.log('web3: id from bytes : ', web3id2)
             assert.strictEqual(soid2, web3id2, 'Two unique Id methods shall generate the same hash')
         })
