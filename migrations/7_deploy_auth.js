3cb128b0e6b2b8d290b84fcc329fb7f9e7a2f44b
gongf05 <fang.ucla@gmail.com>
2018-07-15 15:31:38 -0700

    update file name

--- migrations/7_deploy_auth.js
+++ migrations/7_deploy_auth.js
@@ -1,6 +1,6 @@
 /* global artifacts */
 const Market = artifacts.require('Market.sol')
-const AccessControl = artifacts.require('acl.sol')
+const AccessControl = artifacts.require('ACL.sol')
 
 const deployACL = (deployer) => {
     const MarketAddress = Market.address
