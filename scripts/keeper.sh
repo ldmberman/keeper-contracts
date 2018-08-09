29d9ab6d0cd59628b766f452cc365d21c3411efd
Sebastian Gerske <sebastian@oceanprotocol.com>
2018-08-09 15:43:16 +0200

    added working docker file

--- scripts/keeper.sh
+++ scripts/keeper.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export LC_ALL=$(locale -a | grep en_US)
-export LANG=$(locale -a | grep en_US)
+
 ganache-cli  > /dev/null 1> /dev/null &
+
 sleep 5
-truffle compile
-truffle migrate --network development
+
+truffle migrate
+
 tail -f /dev/null
