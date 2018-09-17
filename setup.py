f3723ff99bef03eb58bf7968ddc99eb011610bd0
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-09-17 16:20:02 +0200

    Bumperd version to 0.0.2

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/keeper-contracts',
-    version='0.0.1.2',
+    version='0.0.2',
     zip_safe=False,
 )
 
